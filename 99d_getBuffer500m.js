15e185e1cab47333e96a85068a58549141586c24
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2026-05-20 17:20:58 -0300

    Create 99d_getBuffer500m.js

---
+++ 99d_getBuffer500m.js
@@ -0,0 +1,123 @@
+// get area by territory 
+
+// an adaptation from:
+// calculate area of @author João Siqueira
+
+// define root imageCollection
+var root = 'projects/ee-deisejunqueira/assets/AUGI-MAP/classification/';
+
+// define files to process 
+var asset = [
+  //root + 'depressaoPeriferica_classification_v2',
+  //root + 'depressaoPeriferica_classification_SENTINEL_v2',
+  //root + 'depressaoPeriferica_classification_EMBEDDINGS_v2',
+  //root + 'depressaoPeriferica_classification_EMBEDDINGS_v2_temporal_v2',
+  root + 'depressaoPeriferica_classification_EMBEDDINGS_v2_temporal_v2_spatial_v2'
+];
+
+
+// define classification regions
+// read study area
+var carta = ee.FeatureCollection('projects/ee-deisejunqueira/assets/DepressaoPeriferica');
+var territory = ee.Image().paint(carta, 'id').rename('territory');
+
+// plot regions
+Map.addLayer(territory.randomVisualizer());
+
+var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10_1/mapbiomas_brazil_collection10_1_coverage_v1')
+
+
+// change the scale if you need.
+var scale = 10;
+
+// define the years to bem computed 
+var years = ee.List.sequence({'start': 2017, 'end': 2024, 'step': 1}).getInfo();
+
+// define a Google Drive output folder 
+var driverFolder = 'AREA-EXPORT-WETLANDS-SP';
+
+// for each file 
+asset.map(function(file) {
+  // get the classification for the file[i] 
+  var asset_i = collection;
+  var toBuff = ee.Image(file);
+  
+  // set only the basename
+  var basename = file.slice(ee.String(root).length().getInfo()) + '_buffer500m';
+  
+  // Image area in km2
+  var pixelArea = ee.Image.pixelArea().divide(10000);
+  
+  // Geometry to export
+  var geometry = asset_i.geometry();
+  
+  // convert a complex object to a simple feature collection 
+  var convert2table = function (obj) {
+    obj = ee.Dictionary(obj);
+      var territory = obj.get('territory');
+      var classesAndAreas = ee.List(obj.get('groups'));
+      
+      var tableRows = classesAndAreas.map(
+          function (classAndArea) {
+              classAndArea = ee.Dictionary(classAndArea);
+              var classId = classAndArea.get('class');
+              var area = classAndArea.get('sum');
+              var tableColumns = ee.Feature(null)
+                  .set('territory', territory)
+                  .set('class_id', classId)
+                  .set('area', area)
+                  .set('file', basename);
+                  
+              return tableColumns;
+          }
+      );
+  
+      return ee.FeatureCollection(ee.List(tableRows));
+  };
+  
+  // compute the area
+  var calculateArea = function (image, territory, geometry) {
+      var territotiesData = pixelArea.addBands(territory).addBands(image)
+          .reduceRegion({
+              reducer: ee.Reducer.sum().group(1, 'class').group(1, 'territory'),
+              geometry: geometry,
+              scale: scale,
+              maxPixels: 1e13
+          });
+          
+      territotiesData = ee.List(territotiesData.get('groups'));
+      var areas = territotiesData.map(convert2table);
+      areas = ee.FeatureCollection(areas).flatten();
+      return areas;
+  };
+  
+  // perform per year 
+  var areas = years.map(
+      function (year) {
+          var image_i = asset_i.select('classification_' + year);
+          
+           var buffer = toBuff.select('classification_' + year).eq(9)
+            .distance(ee.Kernel.euclidean(500, 'meters'), false);
+            
+          var image = image_i.updateMask(buffer)
+          
+          var areas = calculateArea(image, territory, geometry);
+          // set additional properties
+          areas = areas.map(
+              function (feature) {
+                  return feature.set('year', year);
+              }
+          );
+          return areas;
+      }
+  );
+  
+  areas = ee.FeatureCollection(areas).flatten();
+  
+  Export.table.toDrive({
+      collection: areas,
+      description: 'AREA-' + basename,
+      folder: driverFolder,
+      fileFormat: 'CSV'
+  });
+});
